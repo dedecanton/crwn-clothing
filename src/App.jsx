a817ae76a162140f8b26295598d80efa43c01fdf
dedecanton <andrecanton18@gmail.com>
2022-06-13 15:27:08 -0300

    feat: section 4 completed

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Directory from "./components/directory/directory.component";
+
+const App = () => {
+  return (
+    <Directory/>
+  );
+};
+
+export default App;
